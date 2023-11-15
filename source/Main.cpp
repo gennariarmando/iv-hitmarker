069a6c9e9949ff4fe33fcee3dbb4d6b1bd2f205d
_AG <gennariarmando@outlook.com>
2023-11-15 19:47:30 +0100

    Little code revision

--- source/Main.cpp
+++ source/Main.cpp
@@ -17,13 +17,11 @@ using namespace plugin;
 class HitMarkerIV {
 public:
     static inline CSprite2d hitmarkerSprite = {};
-    static inline int32_t targetDisplayTime = 0;
+    static inline uint32_t targetDisplayTime = 0;
     static inline CPed* targetPed = nullptr;
     static inline bool targetHeadShot = false;
 
     HitMarkerIV() {
-        plugin::InitEverything();
-
         plugin::Events::initEngineEvent += []() {
             int32_t slot = CTxdStore::AddTxdSlot("hitmarker");
             CTxdStore::LoadTxd(slot, "platform:/textures/hitmarker");
@@ -48,6 +46,13 @@ public:
 
             auto base = new T_CB_Generic_NoArgs([]() {
                 auto playa = FindPlayerPed(0);
+                if (!playa)
+                    return;
+
+                bool isDotShowing = (CHud::Components[aHudComponentInfo[HUD_WEAPON_DOT].m_nIndex]->IsDisplaying()) || (CHud::Components[aHudComponentInfo[HUD_WEAPON_SCOPE].m_nIndex]->IsDisplaying());
+                if (!isDotShowing)
+                    return;
+
                 CTaskSimpleAimGun* taskSimpleAimGun = playa->m_pPedIntelligence->m_TaskMgr.FindActiveTaskByType<CTaskSimpleAimGun>(6);
 
                 if (taskSimpleAimGun) {
@@ -60,17 +65,13 @@ public:
                     if (targetDisplayTime > CTimer::GetTimeInMilliseconds()) {
                         rage::Color32 col = CHudColours::Get(HUD_COLOUR_WHITE, 255);
 
-                        if (targetHeadShot) {
-                            if (targetDisplayTime < CTimer::GetTimeInMilliseconds() + 400)
-                                col = CHudColours::Get(HUD_COLOUR_REDDARK, 255);
-                        }
-                        else
+                        if (targetHeadShot)
                             col = CHudColours::Get(HUD_COLOUR_REDDARK, 255);
 
-                        float x = (SCREEN_WIDTH / 2) + ScaleX(0.5f);
-                        float y = (SCREEN_HEIGHT / 2) + ScaleY(0.5f);
-                        float w = ScaleX(4.0f);
-                        float h = ScaleY(4.0f);
+                        float x = (SCREEN_WIDTH / 2);
+                        float y = (SCREEN_HEIGHT / 2);
+                        float w = ScaleX(5.0f);
+                        float h = ScaleY(5.0f);
                         if (hitmarkerSprite.m_pTexture) {
                             hitmarkerSprite.Push();
                             CSprite2d::Draw(rage::Vector4(x - w, y - h, x + w, y + h), col);
